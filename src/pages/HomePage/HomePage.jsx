abd37ba00776b3a9a1d2a416e13754f213d489b4
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-08-08 00:01:35 +0300

    upd

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,25 @@
+import { NavLink } from 'react-router-dom';
+import css from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.wrapper}>
+        <div className={css.wrapperContent}>
+          <h1 className={css.title}>Campers Rental</h1>
+          <p className={css.paragraph}>
+            Discover the freedom of the open road and the beauty of nature with
+            our top-of-the-line camper rentals. Whether you’re planning a
+            weekend getaway or an epic cross-country journey, we have the
+            perfect camper to suit your needs.
+          </p>
+        </div>
+        <NavLink to="/catalog" className={css.btn}>
+          Start Your Adventure
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
