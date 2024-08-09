40ebd777f2b805a029c8e462f36c698cb88f9a3f
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-08-10 00:58:23 +0300

    upd

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,5 +1,122 @@
+import { useForm } from 'react-hook-form';
+import css from './BookingForm.module.css';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import clsx from 'clsx';
+import Icon from '../../shared/Icon/Icon';
+import { useEffect, useRef } from 'react';
+import Flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 const BookingForm = () => {
-  return <div>BookingForm</div>;
+  const dateInputRef = useRef(null);
+  let flatpickrInstance = useRef(null);
+
+  useEffect(() => {
+    if (dateInputRef.current) {
+      flatpickrInstance.current = Flatpickr(dateInputRef.current, {
+        dateFormat: 'Y-m-d',
+      });
+    }
+  }, []);
+
+  const handleClickCalendar = () => {
+    if (flatpickrInstance.current) {
+      flatpickrInstance.current.open();
+    }
+  };
+
+  const FormSchema = Yup.object({
+    name: Yup.string()
+      .min(2, 'must contain at least 2 characters')
+      .max(64)
+      .required(),
+    email: Yup.string().email().required(),
+    bookingDate: Yup.string(),
+    comment: Yup.string().max(120),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(FormSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      name: '',
+      email: '',
+      bookingDate: '',
+      comment: '',
+    },
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <div className={css.wrapper}>
+      <h3 className={css.title}>Book your campervan now</h3>
+      <p className={css.text}>
+        Stay connected! We are always ready to help you.
+      </p>
+      <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
+        <label className={css.labelWrapper}>
+          <input
+            type="text"
+            {...register('name')}
+            placeholder="Name"
+            className={clsx(css.input, { [css.inputError]: errors.name })}
+          />
+          <p className={css.errorMessage}>{errors.name?.message}</p>
+        </label>
+        <label className={css.labelWrapper}>
+          <input
+            type="email"
+            {...register('email')}
+            placeholder="Email"
+            className={clsx(css.input, { [css.inputError]: errors.email })}
+          />
+          <p className={css.errorMessage}>{errors.email?.message}</p>
+        </label>
+        <label className={css.labelWrapper}>
+          <input
+            type="text"
+            {...register('bookingDate')}
+            placeholder="Booking date"
+            ref={dateInputRef}
+            className={clsx(css.input, {
+              [css.inputError]: errors.bookingDate,
+            })}
+          />
+          <button
+            className={css.btnCalendar}
+            type="button"
+            onClick={handleClickCalendar}
+          >
+            <Icon className={css.icon} iconId="icon-calendar" />
+          </button>
+          <p className={css.errorMessage}>{errors.bookingDate?.message}</p>
+        </label>
+        <label className={css.labelWrapper}>
+          <textarea
+            {...register('comment')}
+            placeholder="Comment"
+            className={clsx(css.input, css.textarea, {
+              [css.inputError]: errors.comment,
+            })}
+          ></textarea>
+          <p className={css.errorMessage}>{errors.comment?.message}</p>
+        </label>
+        <button type="submit" className={css.btn}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default BookingForm;
--- src/components/VehicleDetail/VehicleDetail.jsx
+++ src/components/VehicleDetail/VehicleDetail.jsx
@@ -7,30 +7,32 @@ const VehicleDetail = ({ data }) => {
       <div className={css.divider}></div>
       <div className={css.wrapper}>
         <table className={css.table}>
-          <tr className={css.item}>
-            <td>Form</td>
-            <td>{data.form}</td>
-          </tr>
-          <tr className={css.item}>
-            <td>Length</td>
-            <td>{data.length}</td>
-          </tr>
-          <tr className={css.item}>
-            <td>Width</td>
-            <td>{data.width}</td>
-          </tr>
-          <tr className={css.item}>
-            <td>Height</td>
-            <td>{data.height}</td>
-          </tr>
-          <tr className={css.item}>
-            <td>Tank</td>
-            <td>{data.tank}</td>
-          </tr>
-          <tr className={css.item}>
-            <td>Consumption</td>
-            <td>{data.consumption}</td>
-          </tr>
+          <tbody>
+            <tr className={css.item}>
+              <td>Form</td>
+              <td>{data.form}</td>
+            </tr>
+            <tr className={css.item}>
+              <td>Length</td>
+              <td>{data.length}</td>
+            </tr>
+            <tr className={css.item}>
+              <td>Width</td>
+              <td>{data.width}</td>
+            </tr>
+            <tr className={css.item}>
+              <td>Height</td>
+              <td>{data.height}</td>
+            </tr>
+            <tr className={css.item}>
+              <td>Tank</td>
+              <td>{data.tank}</td>
+            </tr>
+            <tr className={css.item}>
+              <td>Consumption</td>
+              <td>{data.consumption}</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
